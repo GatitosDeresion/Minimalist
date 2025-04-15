db4ba221e353a97d17bb9cbb3f03debb11f05b7e
GatitosDeresion <doal03@icloud.com>
2025-04-14 21:00:13 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const toggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('nav');
+<script>
+  const toggle = document.getElementById('menu-toggle');
+  const nav = document.getElementById('nav');
 
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+</script>
